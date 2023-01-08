fb298eea5e7b2a2cef9d0031494c68e7d4a2ed68
AliDayi97 <alidayi97@hotmail.com.tr>
2023-01-08 13:29:20 +0200

    added new files

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+type HomePageProps = {};
+
+const HomePage = ({}: HomePageProps) => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1>Welcome to the NextJs, Mantine and Tailwind template</h1>
+    </div>
+  );
+};
+
+export default HomePage;
